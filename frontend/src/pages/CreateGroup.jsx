4cf937d09b8a18cfa068ddd5291d2552e14b5437
Priyatriathi <priyatripathi0415@gmail.com>
2025-05-25 21:17:19 +0530

    new page added

---
+++ frontend/src/pages/CreateGroup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateGroup = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Create Group</h1>
+    </div>
+  )
+}
+
+export default CreateGroup
